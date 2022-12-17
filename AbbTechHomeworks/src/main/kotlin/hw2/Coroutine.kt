05f85ef0d33117110d0a10bf395657c6b76d2830
mahabbatcomardli <mahabbat.comardli@ibar.az>
2022-12-17 22:45:37 +0400

    changed

--- AbbTechHomeworks/src/main/kotlin/hw2/Coroutine.kt
+++ AbbTechHomeworks/src/main/kotlin/hw2/Coroutine.kt
@@ -17,7 +17,7 @@ fun runSync() {
     println("hw2.runSync method.")
     //  launch 1000 coroutines. Invoke hw2.doWork(index/number of coroutine) one after another. Example 1, 2, 3, 4, 5, etc.
 
-    for (i in 1..30) {
+    for (i in 1..1000) {
         runBlocking {
             doWork(i.toString())
         }
@@ -28,7 +28,7 @@ fun runAsync() {
     println("hw2.runAsync method.")
     //  launch 1000 coroutines. Invoke hw2.doWork(index/number of coroutine) in async way. Example 1, 2, 5, 3, 4, 8, etc.
 
-    for (i in 1..30) {
+    for (i in 1..1000) {
         CoroutineScope(Dispatchers.IO).launch {
             doWork(i.toString())
         }
